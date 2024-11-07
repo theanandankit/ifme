4ce825e18e817573c321bcbb5c7dbdfd5907a614
nikvin15 <110590898+nikvin15@users.noreply.github.com>
2024-11-07 20:15:51 +0530

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,7 @@
 #  remember_created_at    :datetime
 #  sign_in_count          :integer          default(0), not null
 #  current_sign_in_at     :datetime
+
 #  last_sign_in_at        :datetime
 #  current_sign_in_ip     :string
 #  last_sign_in_ip        :string
